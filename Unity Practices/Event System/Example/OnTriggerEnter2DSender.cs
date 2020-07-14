347e59d811995608a9ed055b86df9bef84b30a4f
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2020-07-15 00:05:00 +0300

    Add files via upload

---
+++ Unity Practices/Event System/Example/OnTriggerEnter2DSender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extentions;
+
+[RequireComponent(typeof(Collider2D))]
+public class OnTriggerEnter2DSender : TriggerCommand
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<Player>() != null)
+        {
+            if (isTriggered && oneShot)
+                return;
+            Send();
+        }
+    }
+}
---
+++ Unity Practices/Event System/GameCommandReciever.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCommandReciever : MonoBehaviour
+{
+    private Dictionary<TriggerCommandsType, List<Action>> _handlers = new Dictionary<TriggerCommandsType, List<Action>>();
+
+    public void Recieve(TriggerCommandsType command)
+    {
+        List<Action> callbacks = null;
+        if (_handlers.TryGetValue(command, out callbacks))
+        {
+            foreach (var methodToInvoke in callbacks)
+            {
+                methodToInvoke();
+            }
+        }
+    }
+
+    public void Register(TriggerCommandsType command, GameCommandHandler handler)
+    {
+        List<Action> callbacks = null;
+        if (!_handlers.TryGetValue(command, out callbacks))
+        {
+            callbacks = _handlers[command] = new List<Action>();
+        }
+        callbacks.Add(handler.OnInteraction);
+    }
+
+    public void Remove(TriggerCommandsType command, GameCommandHandler handler)
+    {
+        _handlers[command].Remove(handler.OnInteraction);
+    }
+}
